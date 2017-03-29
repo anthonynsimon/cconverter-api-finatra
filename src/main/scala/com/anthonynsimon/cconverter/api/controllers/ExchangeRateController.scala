6abb1bfacdf466f862782f4e41b875aaf766b544
Anthony Najjar <anthonynsimon@gmail.com>
2017-03-29 17:37:24 +0200

    Remove blocking code on controller

--- src/main/scala/com/anthonynsimon/cconverter/api/controllers/ExchangeRateController.scala
+++ src/main/scala/com/anthonynsimon/cconverter/api/controllers/ExchangeRateController.scala
@@ -5,7 +5,6 @@ import com.anthonynsimon.cconverter.api.services.ExchangeRateService
 import com.google.inject.Inject
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
-import com.twitter.util.Await
 
 class ExchangeRateController @Inject()(exchangeRateService: ExchangeRateService) extends Controller {
 
@@ -16,17 +15,20 @@ class ExchangeRateController @Inject()(exchangeRateService: ExchangeRateService)
 		}
 
 		get("/convert") { request: CurrencyConvertRequest =>
-			val rates = Await.result(exchangeRateService.getRates(request.from))
-			val exchangeRate = if (request.from == request.to) BigDecimal.valueOf(1L) else rates.rates(request.to)
-			val conversionResult = request.amount * exchangeRate
+			exchangeRateService
+					.getRates(request.from)
+					.map[CurrencyConversionResult](rates => {
 
-			CurrencyConversionResult(
-				fromCurrency = request.from,
-				toCurrency = request.to,
-				amountToConvert = request.amount,
-				conversionResult = conversionResult,
-				exchangeRate = exchangeRate
-			)
+				val exchangeRate = if (request.from == request.to) BigDecimal.valueOf(1L) else rates.rates(request.to)
+				val conversionResult = request.amount * exchangeRate
+
+				CurrencyConversionResult(
+					fromCurrency = request.from,
+					toCurrency = request.to,
+					amountToConvert = request.amount,
+					conversionResult = conversionResult,
+					exchangeRate = exchangeRate)
+			})
 		}
 	}
 }
